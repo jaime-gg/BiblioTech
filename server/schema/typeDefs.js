c4459ad54811b9c006f78afb8f5f188a64f98471
jester <jaimebuddy98@gmail.com>
2022-04-14 13:50:02 -0500

    basic outline

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,10 @@
+// IMPORT THE GQL TAGGED TEMPLATE FUNCTION
+const { gql } = require('apollo-server-express');
+
+// CREATE OUR TYPE-DEFS
+const typeDefs = gql`
+
+`;
+
+// EXPORT THE TYPE-DEFS
+module.exports = typeDefs;
