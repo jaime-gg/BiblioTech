6d6da898c39fa232ae745ac439c42962b650bfc6
jester <jaimebuddy98@gmail.com>
2022-04-14 18:26:52 -0500

    fixed save book bug

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -72,11 +72,10 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      const { data } = await saveBook({
+        variables: { bookData: { ...bookToSave } },
+      });
+      console.log(savedBookIds);
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
