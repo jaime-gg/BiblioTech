5345d0d194442ab971fb3c5b4c4ed3c82a0e4aa6
jester <jaimebuddy98@gmail.com>
2022-04-14 15:51:37 -0500

    added needed type defs

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -3,6 +3,33 @@ const { gql } = require('apollo-server-express');
 
 // CREATE OUR TYPE-DEFS
 const typeDefs = gql`
+    type Auth {
+        token: ID!
+        user: User
+    }
+    type User {
+        _id: ID!
+        username: String!
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+    type Book {
+        bookId: ID!
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String!
+    }
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
 
     type Query {
         me: User
