0f21f2b102428c33727d29d4c2ca40443af43caa
jester <jaimebuddy98@gmail.com>
2022-04-14 15:44:58 -0500

    query and mutation

--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -4,6 +4,15 @@ const { gql } = require('apollo-server-express');
 // CREATE OUR TYPE-DEFS
 const typeDefs = gql`
 
+    type Query {
+        me: User
+    }
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: ID!): User
+    }
 `;
 
 // EXPORT THE TYPE-DEFS
